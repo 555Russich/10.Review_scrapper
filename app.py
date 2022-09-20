b54f68183d8282c8aead8321ba586a5456552d56
russich555 <Kutsokondima@yandex.ru>
2022-09-20 18:30:48 +0300

    Rename window header

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Парсер отзывов"))
         self.input_url.setText(_translate("MainWindow", "Введите URL"))
         self.button_collect_data.setText(_translate("MainWindow", "Пуск"))
 
